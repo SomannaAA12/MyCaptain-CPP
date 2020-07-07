7b789d4ce1cbcf6b57d0121cfc49ea34f81f50b6
SomannaAA12 <67817939+SomannaAA12@users.noreply.github.com>
2020-07-07 11:01:27 +0530

    product.cpp
    This program gives the product of two numbers entered by the user.

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double a, b, product;
+	cout<< "Enter two numbers: ";
+	cin>> a>> b;
+	product = a*b;
+	cout<< "The product is: "<< product;
+	return 0; 
+}
